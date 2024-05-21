bb2de6ba1ec980e484f16a2afda69dd0632fee88
Pranshu Yadav <yadavpranshu743@gmail.com>
2024-05-21 21:46:43 +0530

    first commit

---
+++ java project/c.java
@@ -0,0 +1,10 @@
+class c { 
+    public static void main (String[] args){ 
+    String s = "Welcome to Galgotias"; 
+    int x = 2; 
+    do { 
+    System.out.print(s.charAt(x)); 
+    x++ ; 
+    } while (x < 2); 
+    } 
+    }
